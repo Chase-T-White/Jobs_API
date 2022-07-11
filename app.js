9829eb9531d14c90e13cb1cff37a8426215c38b4
Chase White <chase.t.white@hotmail.com>
2022-07-11 17:44:50 -0600

    Created routes and controllers

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
+// Connect routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+// Connect Database
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
 
 // Middlewares
 app.use(express.json());
@@ -13,9 +17,8 @@ app.use(express.json());
 // Extra Packages
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Jobs API');
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
@@ -23,11 +26,13 @@ app.use(notFound);
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
-    } catch (err) {
-        console.log(err);
-    };
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 start();

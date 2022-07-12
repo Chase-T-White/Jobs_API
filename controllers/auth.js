0ecbd48578270a10f609bf0f23076975cb46d78f
Chase White <chase.t.white@hotmail.com>
2022-07-12 14:08:14 -0600

    Created encryption and validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,62 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/bad-request");
+const { UnauthenticatedError } = require("../errors/unauthenticated");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-    res.send('register user');
+  //   Optional validator within the controller   //
+
+  //   const { name, email, password } = req.body;
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError("Please provide name, email and password");
+  //   }
+
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  const user = await User.create({ ...tempUser });
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRATION }
+  );
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-    res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRATION }
+  );
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
 };
